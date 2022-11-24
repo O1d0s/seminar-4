bacab7245c68f2ceb78a77fad617ffad69b1da7f
Aidos <ajdos0735@gmaul.com>
2022-11-24 21:19:54 +0600

    Сделал 25 задачу

---
+++ Zadacha-25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("A = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("B = ");
+int b = int.Parse(Console.ReadLine());
+ 
+Console.WriteLine();
+Console.WriteLine(Step(a,b));
+
+int Step(int num1, int num2){
+    int num3 = num1;
+    for(int i=1;i<num2;i++){
+        num1 *= num3;
+    }
+    return num1;
+}
