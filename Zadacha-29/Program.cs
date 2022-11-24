c1fce2e14176a4c42db33b306f99e0161f374d02
Aidos <ajdos0735@gmaul.com>
2022-11-24 21:44:04 +0600

    Сделал 29 задачу

---
+++ Zadacha-29/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = RadMas(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] RadMas(int size){
+    int[] massiv = new int[size];
+    for(int i=0;i<size;i++){
+        massiv[i] = new Random().Next(0,100);
+    }
+    return massiv;
+}
