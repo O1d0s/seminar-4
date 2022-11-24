8bdf7bbf395e6bd480c91e65e8d01fe4d640cff1
Aidos <ajdos0735@gmaul.com>
2022-11-24 21:32:51 +0600

    Сделал 27 задачу

---
+++ Zadacha-27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Вводите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Sum(num));
+
+int Sum(int n){
+    int s=0,k=0;
+    while (n!=0){
+        k=n%10;
+        s+=k;
+        n/=10;
+    }
+    return s;
+}
